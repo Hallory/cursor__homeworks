18edfe2d2e672022b2384b0492b4c43b0da64505
Hallory <55157489+Hallory@users.noreply.github.com>
2023-07-01 16:51:37 +0200

    Add files via upload

---
+++ homework3/main.js
@@ -0,0 +1,24 @@
+// function getMaxDigit(number){
+//     let maxDigit = 0;
+
+//     const numberStr = number.toString();
+
+//     for (let i = 0; i < numberStr.length; i++) {
+//         const element = numberStr[i];
+//         if(maxDigit<element){
+//             maxDigit = element
+//         }
+//     }
+//     console.log(maxDigit)
+// }
+// getMaxDigit(14252);
+
+// function numberPow(number){
+//     let powNumb = number.toString();
+//     for (let i = 0; i < powNumb.length; i*i) {
+//         const element = [i];
+        
+//     }
+// }
+
+
